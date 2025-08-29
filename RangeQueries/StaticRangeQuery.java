b9e5a1ad25e7edfe889d9e8d3383f0917744ae7e
surajprajapati1538 <spsuraj0640481@gmail.com>
2025-08-29 12:25:02 +0530

    added print statement

--- RangeQueries/StaticRangeQuery.java
+++ RangeQueries/StaticRangeQuery.java
@@ -2,6 +2,7 @@ import java.io.*;
 
 public class StaticRangeQuery {
     public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter out = new PrintWriter(System.out);
 
